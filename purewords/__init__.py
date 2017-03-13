0f5782ba9e282a276b43aa3581ae6e9b7e20fa3d
plliao <r02921027@ntu.edu.tw>
2017-03-13 16:26:50 +0800

    remove config

--- purewords/__init__.py
+++ purewords/__init__.py
@@ -1,25 +1,36 @@
 import re
 import os.path
-import importlib.util
-import yaml
+import importlib
 
 from .preprocessing import remove_meaning_notation
 from .preprocessing import remove_stopwords
 from .preprocessing import split_document
 from .preprocessing import cut_sentence
 
-purewords_path = os.path.abspath(__file__)
-purewords_dir = os.path.dirname(purewords_path)
 
 class PureWords(object):
 
-    def __init__(self, config_path=None):
-        if config_path is None:
-            config_path = os.path.join(purewords_dir, 'configs/default.yml')
-        config_file = open(config_path, 'r')
+    def __init__(self, tokenizer='yoctol_jieba',
+            remove_url=True, remove_time=True,
+            remove_phone_number=True, replace_title=True,
+            replace_abbreviation=True, remove_angle_brackets=True,
+            stopwords_path='configs/stopwords.txt', max_len=200,
+            min_len=1
+        ):
+        self.config = {}
+        self.config['tokenizer'] = tokenizer
+        self.config['remove_url'] = remove_url
+        self.config['remove_time'] = remove_time
+        self.config['remove_phone_number'] = remove_phone_number
+        self.config['replace_title'] = replace_title
+        self.config['replace_abbreviation'] = replace_abbreviation
+        self.config['remove_angle_brackets'] = remove_angle_brackets
+        self.config['stopwords_path'] = stopwords_path
+        self.config['max_len'] = max_len
+        self.config['min_len'] = min_len
 
-        self.config = yaml.load(config_file.read())
-        config_file.close()
+        purewords_path = os.path.abspath(__file__)
+        purewords_dir = os.path.dirname(purewords_path)
 
         stopwords_file = open(
             os.path.join(purewords_dir, self.config['stopwords_path']), 'r'
---
+++ purewords/tokenizer/whitespace_tokenizer.py
@@ -0,0 +1,5 @@
+
+class whitespace_tokenizer(object):
+
+    def cut(self, sentence):
+        return sentence.split(' ')
