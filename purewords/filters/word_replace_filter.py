61e997334018627accffc582cb475531d6671672
plliao <r02921027@ntu.edu.tw>
2017-06-28 11:41:34 +0800

    add filter init

---
+++ purewords/filters/word_replace_filter.py
@@ -0,0 +1,21 @@
+'''Word replace base filter'''
+import re
+
+from .base_filter import BaseFilter
+
+class WordReplaceFilter(BaseFilter):
+
+    def __init__(self, replace_dictionary={}):
+        self.replace_dictionary = replace_dictionary
+
+    def add_replacement(self, word, replacement=''):
+        self.replace_dictionary[word] = replacement
+
+    def __call__(self, sentence):
+        for pattern, replacement in self.replace_dictionary.items():
+            sentence = re.sub(
+                pattern,
+                replacement,
+                sentence
+            )
+        return senten
