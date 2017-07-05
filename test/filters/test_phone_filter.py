e1d741d87222147867e2b59281c573adbd5c6f9b
Solumilken <s916526000@gmail.com>
2017-07-05 12:30:47 +0800

    temperally add invertible test

--- test/filters/test_phone_filter.py
+++ test/filters/test_phone_filter.py
@@ -1,18 +1,32 @@
-'''Phone filter testcases'''
+'''Phone filter_ testcases'''
 from unittest import TestCase
 
 from purewords.filters import phone_filter
 
+
 class TestPhoneFilterClass(TestCase):
 
     def setUp(self):
-        self.filter = phone_filter
+        self.filter_ = phone_filter
 
-    def test_phone_filter(self):
+    def test_phone_filter_(self):
         sentence = "薄餡手機是:0912345678, " \
                    + "家電請打:02-2266-2266或08-449-5978"
         answer = "薄餡手機是:_phone_, 家電請打:_phone_或_phone_"
         self.assertEqual(
             answer,
-            self.filter(sentence)
+            self.filter_(sentence)
         )
+
+    # def test_invertible(self):
+    #     sentence = "薄餡手機是:0912345678, " \
+    #                + "家電請打:02-2266-2266或08-449-5978"
+    #     answer = "薄餡手機是:_phone_, 家電請打:_phone_或_phone_"
+    #     self.assertEqual(
+    #         answer,
+    #         self.filter_.go_forward(sentence, False)
+    #     )
+    #     self.assertEqual(
+    #         sentence,
+    #         self.filter_.go_backward(answer, False)
+    #     )
